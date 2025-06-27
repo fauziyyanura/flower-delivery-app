e0c5a587976bf5f20e7384cb74661832ce165b8c
Your Name <fauziyyanuraahmad@gmail.com>
2025-06-27 10:52:53 +0100

    Update App.js layout for product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import UtilityNav from "./components/UtilityNav";
 import FreshFlowersPage from "./components/FreshFlowersPage";
 import DriedFlowersPage from "./components/DriedFlowerPage";
 import LivePlantsPage from "./components/LivePlantPage";
+import AromaCandlePage from "./components/AromaCandlePage";
 import Footer from "./components/Footer";
 
 
@@ -17,7 +18,7 @@ function App() {
         <Route path="/shop/fresh-flowers" element={<FreshFlowersPage />} />
         <Route path="/shop/dried-flowers" element={<DriedFlowersPage />} />
         <Route path="/shop/live-plants" element={<LivePlantsPage />}  />
-
+        <Route path="/shop/aroma-candles" element={<AromaCandlePage />} />
       </Routes>
       <Footer />
     </>
