96769d8fea95549d0880dc0bebe6a61115a3a3ab
Your Name <fauziyyanuraahmad@gmail.com>
2025-07-18 15:44:08 +0100

    Added changes to signin and nav

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./signup.css";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
